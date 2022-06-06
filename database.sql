d1c879d2bb407cf89974413c5795f65a6049ddf7
Hieu Ha Trung <hatrunghieuhpvn@gmail.com>
2022-06-06 12:54:34 +0700

    add targets

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `targets` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `domain` VARCHAR(255) NULL,
+    `subdomain` VARCHAR(255) NULL,
+    `technology` VARCHAR(255) NULL,
+    `created` DATE NULL
+);
